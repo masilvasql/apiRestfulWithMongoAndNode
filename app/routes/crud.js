35a43fe90a3ea3bbfc48fbc290783090a8218ffc
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-04-27 16:34:24 -0300

    finalização das apis de crud

--- app/routes/crud.js
+++ app/routes/crud.js
@@ -20,13 +20,47 @@ rota.route('/produtos')
             res.json({ message: "Produto salvo com sucesso" })
         })
     })
-    .get((req,res)=>{
-        Produto.find((err, prod)=>{
-            if(err)
+    .get((req, res) => {
+        Produto.find((err, prod) => {
+            if (err)
                 res.send(`Erro ao buscar produtos ${err}`)
             res.send(prod)
         })
     })
+
+rota.route("/produtos/produto_id")
+    .get((req, res) => {
+        Produto.findById(req.body.id, (err, produto) => {
+            if (err)
+                res.send("Erro: ", err)
+            res.send(produto)
+        })
+    })
+    .put((req, res) => {
+        Produto.findById(req.body.id, (err, produto) => {
+            if (err)
+                res.send("Produto não encontrado ", err)
+            produto.nome = req.body.nome
+            produto.preco = req.body.preco
+            produto.descricao = req.body.descricao
+
+            produto.save((err) => {
+                if (err)
+                    res.send("Falha ao atualizar os dados ", err)
+                res.send("Produto salvo com sucesso")
+            })
+        })
+    })
+    .delete((req, res) => {
+        Produto.deleteOne({
+            _id: req.body.id
+        }, (err) => {
+            if (err)
+                res.send("Erro ao deletar item")
+            res.send("Produto deletado com sucesso")
+        })
+    })
+
 
 rota.get('/', (req, res) => {
     res.json({ message: "Olá, deu boa" })
